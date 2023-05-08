313a8e5b448e792b1bbf3209388a4499aa7c5162
antonio-mz <antmonlop4@alum.us.es>
2023-05-08 20:33:41 +0200

    Comment- hecho

--- GitMinerTemplate-main/src/main/java/aiss/gitminer/Exception/CommentNotFoundException.java
+++ GitMinerTemplate-main/src/main/java/aiss/gitminer/Exception/CommentNotFoundException.java
@@ -1,4 +1,8 @@
 package aiss.gitminer.Exception;
 
-public class CommentNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Comment not found")
+public class CommentNotFoundException extends Exception{
 }
